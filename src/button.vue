a68e62ae0ef78332724704833bbb7712cc762bf1
Lxh6688 <1272662790@qq.com>
2022-05-22 15:36:55 +0800

    验证 iconPosition 为 left 或 right 其中的一个

--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,16 @@
 
 <script>
 export default {
-  props: ['icon', 'iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value){
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
 }
 </script>
 
