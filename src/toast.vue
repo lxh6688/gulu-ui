8bd9313060f2c3ad919820d51b6272d133c0e2f9
Lxh6688 <1272662790@qq.com>
2022-06-12 22:45:07 +0800

    改造 autoclose 属性

--- src/toast.vue
+++ src/toast.vue
@@ -17,12 +17,11 @@ export default {
   name: "GuluToast",
   props: {
     autoClose: {
-      type: Boolean,
-      default: true,
-    },
-    autoCloseDelay: {
-      type: Number,
+      type: [Boolean, Number],
       default: 5,
+      validator(value) {
+        return value === false || typeof value === "number";
+      },
     },
     closeButton: {
       type: Object,
@@ -68,7 +67,7 @@ export default {
       if (this.autoClose) {
         setTimeout(() => {
           this.close();
-        }, this.autoCloseDelay * 1000);
+        }, this.autoClose * 1000);
       }
     },
     close() {
