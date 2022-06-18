b6956fe7ba193dbc3827c93e3e3837219b91cf3f
Lxh6688 <1272662790@qq.com>
2022-06-18 17:42:36 +0800

    功能方面基本完场

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" ref="contentWrapper" v-if="visible">
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -10,44 +10,63 @@
 </template>
 
 <script>
-  export default {
-    name: "GuluPopover",
-    data () {
-      return {visible: false}
+export default {
+  name: "GuluPopover",
+  data() {
+    return { visible: false };
+  },
+  methods: {
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let { width, height, top, left } =
+        this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
     },
-    methods: {
-      xxx () {
-        this.visible = !this.visible
+    onClickDocument(e) {
+      if (
+        this.$refs.popover &&
+        (this.$refs.popover === e.target ||
+          this.$refs.popover.contains(e.target))
+      ) {
+        return;
+      }
+      this.close();
+    },
+    open() {
+      this.visible = true;
+      this.$nextTick(() => {
+        this.positionContent();
+        document.addEventListener("click", this.onClickDocument);
+      });
+    },
+    close() {
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
         if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-            let eventHandler = () => {
-              this.visible = false
-              document.removeEventListener('click', eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
-        }else{
-          console.log('vm 隐藏 popover')
+          this.close();
+        } else {
+          this.open();
         }
       }
-    }
-  }
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .popover {
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
-  }
-  .content-wrapper {
-    position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    transform: translateY(-100%);
-  }
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
 </style> 
