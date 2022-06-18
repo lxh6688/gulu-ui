b803c47ecc51943bd78eb0601d90a33215e9e1b5
Lxh6688 <1272662790@qq.com>
2022-06-18 18:35:45 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,70 +3,94 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
     </span>
   </div>
 </template>
 
 <script>
-export default {
-  name: "GuluPopover",
-  data() {
-    return { visible: false };
-  },
-  methods: {
-    positionContent() {
-      document.body.appendChild(this.$refs.contentWrapper);
-      let { width, height, top, left } =
-        this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
-      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+  export default {
+    name: "GuluPopover",
+    data () {
+      return {visible: false}
     },
-    onClickDocument(e) {
-      if (
-        this.$refs.popover &&
-        (this.$refs.popover === e.target ||
-          this.$refs.popover.contains(e.target))
-      ) {
-        return;
-      }
-      this.close();
-    },
-    open() {
-      this.visible = true;
-      this.$nextTick(() => {
-        this.positionContent();
-        document.addEventListener("click", this.onClickDocument);
-      });
-    },
-    close() {
-      this.visible = false;
-      document.removeEventListener("click", this.onClickDocument);
-    },
-    onClick(event) {
-      if (this.$refs.triggerWrapper.contains(event.target)) {
-        if (this.visible === true) {
-          this.close();
-        } else {
-          this.open();
+    methods: {
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+            console.log('click close')
+          } else {
+            this.open()
+          }
         }
       }
-    },
-  },
-};
+    }
+  }
 </script>
 
 <style scoped lang="scss">
-.popover {
-  display: inline-block;
-  vertical-align: top;
-  position: relative;
-}
-.content-wrapper {
-  position: absolute;
-  border: 1px solid red;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  transform: translateY(-100%);
-}
-</style> 
+  $border-color: #333;
+  $border-radius: 4px;
+  .popover {
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+  }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
+    transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+  }
+</style>
