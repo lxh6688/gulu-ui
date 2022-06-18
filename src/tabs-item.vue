9b4192cd14ce4a71d6251628f608f104acba130a
Lxh6688 <1272662790@qq.com>
2022-06-18 11:05:58 +0800

    完善代码

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,64 +1,70 @@
 <template>
-  <div class="tabs-item" @click="onClick" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
     <slot></slot>
   </div>
 </template>
 <script>
-  export default {
-    name: 'GuluTabsItem',
-     data(){
+export default {
+  name: "GuluTabsItem",
+  data() {
+    return {
+      active: false,
+    };
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String | Number,
+      requierd: true,
+    },
+  },
+  computed: {
+    classes() {
       return {
-        active: false,
-      }
+        active: this.active,
+        disabled: this.disabled,
+      };
     },
-    props: {
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      name: {
-        type: String|Number,
-        requierd: true
-      }
-    },
-    computed: {
-      classes() {
-        return {
-          active: this.active,
-          disabled: this.disabled
-        }
-      }
-    },
-    inject: ['eventBus'],
-    created(){
-      this.eventBus.$on('update:selected', (name) => {
-        this.active = name === this.name
-      })
-    },
-    methods:{
-      onClick(){
-        if (this.disabled) { return }
-        this.eventBus.$emit('update:selected', this.name, this)
-      }
+  },
+  inject: ["eventBus"],
+  created() {
+    if (this.eventBus) {
+      this.eventBus.$on("update:selected", (name) => {
+        this.active = name === this.name;
+      });
     }
-  }
+  },
+  methods: {
+    onClick() {
+      if (this.disabled) {
+        return;
+      }
+      this.eventBus && this.eventBus.$emit("update:selected", this.name, this);
+      this.$emit("click", this);
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-  $blue: blue;
-  $disabled-text-color: grey;
-  .tabs-item {
-    flex-shrink: 0;
-    padding: 0 1em;
-    cursor: pointer;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    &.active{
-      color: $blue;
-      font-weight: bold;
-    }
-    &.disabled {
-      color: $disabled-text-color;
-    }
+$blue: blue;
+$disabled-text-color: grey;
+.tabs-item {
+  flex-shrink: 0;
+  padding: 0 1em;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  &.active {
+    color: $blue;
+    font-weight: bold;
   }
+  &.disabled {
+    color: $disabled-text-color;
+    cursor: not-allowed;
+  }
+}
 </style> 
