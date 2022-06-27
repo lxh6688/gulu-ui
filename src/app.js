14e781618ff413841951c7d2b9be499aa1d3143e
Lxh6688 <1272662790@qq.com>
2022-06-27 21:01:31 +0800

    确定如何渲染内容

--- src/app.js
+++ src/app.js
@@ -55,17 +55,17 @@ new Vue({
           {
             name: '杭州',
             children: [
-              {name: '上城'},
-              {name: '下城'},
-              {name: '江干'},
+              { name: '上城' },
+              { name: '下城' },
+              { name: '江干' },
             ]
           },
           {
             name: '嘉兴',
             children: [
-              {name: '南湖'},
-              {name: '秀洲'},
-              {name: '嘉善'},
+              { name: '南湖' },
+              { name: '秀洲' },
+              { name: '嘉善' },
             ]
           },
         ]
@@ -75,13 +75,24 @@ new Vue({
           {
             name: '福州',
             children: [
-              {name: '鼓楼'},
-              {name: '台江'},
-              {name: '仓山'},
+              { name: '鼓楼' },
+              { name: '台江' },
+              { name: '仓山' },
             ]
           },
         ]
-      }]
+      }, {
+        name: '安徽',
+        children: [{
+          name: '合肥',
+          children: [{
+            name: '瑶海'
+          }, {
+            name: '庐阳'
+          }]
+        }]
+      }
+      ]
     }
   },
   created() {
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
-      <slot></slot>
-    </div>
+    <div class="trigger" @click="popoverVisible = !popoverVisible"></div>
     <div class="popover">
-      <div v-for="item in source" :key="item.name">
-        <cascader-item :sourceItem="item"></cascader-item>
+      <div class="popover" v-if="popoverVisible">
+        <cascader-items :items="source"></cascader-items>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import CascaderItem from "./cascader-item";
+import CascaderItems from "./cascader-items";
 export default {
   name: "GuluCascader",
-  name: "GuluCascader",
-  components: { CascaderItem },
+  components: { CascaderItems },
   props: {
     source: {
       type: Array,
     },
+  },
+  data() {
+    return {
+      popoverVisible: false,
+    };
   },
 };
 </script>
@@ -28,5 +30,18 @@ export default {
 <style scoped lang="scss">
 @import "var";
 .cascader {
+  .trigger {
+    border: 1px solid red;
+    height: 32px;
+    width: 100px;
+  }
+  .popover {
+    border: 2px solid green;
+    height: 200px;
+    display: flex;
+    .label {
+      white-space: nowrap;
+    }
+  }
 }
 </style> 
