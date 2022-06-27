d4ac591893e255df7d1fcc7a74155c155cf8869a
Lxh6688 <1272662790@qq.com>
2022-06-27 21:05:08 +0800

    parcel 渲染 scss 突然变得很慢

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,51 +1,78 @@
 <template>
-  <div class="cascaderItem">
+  <div class="cascaderItem" :style="{ height: height }">
     <div class="left">
-      <div class="label" v-for="item in items" :key="item.name" @click="leftSelected = item">
-        {{item.name}}
+      <div
+        class="label"
+        v-for="item in items"
+        :key="item.name"
+        @click="leftSelected = item"
+      >
+        {{ item.name }}
+        <icon class="icon" v-if="item.children" name="right"></icon>
       </div>
     </div>
     <div class="right" v-if="rightItems">
-      <gulu-cascader-items :items="rightItems"></gulu-cascader-items>
+      <gulu-cascader-items
+        :items="rightItems"
+        :height="height"
+      ></gulu-cascader-items>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "GuluCascaderItems",
-    props: {
-      items: {
-        type: Array
+import Icon from "./icon";
+export default {
+  name: "GuluCascaderItems",
+  components: { Icon },
+  props: {
+    items: {
+      type: Array,
+    },
+    height: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      leftSelected: null,
+    };
+  },
+  computed: {
+    rightItems() {
+      if (this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children;
+      } else {
+        return null;
       }
     },
-    data () {
-      return {
-        leftSelected: null
-      }
-    },
-    computed: {
-      rightItems () {
-        if (this.leftSelected && this.leftSelected.children) {
-          return this.leftSelected.children
-        } else {
-          return null
-        }
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .cascaderItem {
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    .left {
-      border: 1px solid red;
-    }
-    .right {
-      margin-top: -1px;
-    }
+@import "var";
+.cascaderItem {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+   height: 100px;
+  .left {
+    height: 100%;
+      padding: .3em 0;
   }
+  .right {
+    height: 100%;
+      border-left: 1px solid $border-color-light;
+    }
+    .label {
+      padding: .3em 1em;
+      display: flex;
+      align-items: center;
+      .icon {
+        margin-left: 1em;
+        transform: scale(0.5);
+      }
+  }
+}
 </style> 
